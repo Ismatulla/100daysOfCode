5a210f0f84e0a8062652de356797733e1106d767
Ismatulla <ismatilloqoyliyev@gmail.com>
2024-07-01 15:42:25 +0200

    full auth

--- day-14/routs/users.js
+++ day-14/routs/users.js
@@ -3,17 +3,21 @@ const router = express.Router()
 const User = require('../models/user')
 const catchASync = require('../utils/catchAsync');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleWare');
 router.get('/register', (req, res) => {
   res.render('users/register')
 })
-router.post('/register', catchASync(async (req, res) => {
+router.post('/register', catchASync(async (req, res, next) => {
   try {
     const { username, password, email } = req.body
     const user = new User({ username, email })
     const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash("success", 'Welcome to Yelp Camp!')
-    res.redirect('/campgrounds')
+    req.login(registeredUser, err => {
+      if (err) return next(err)
+      req.flash("success", 'Welcome to Yelp Camp!')
+      res.redirect('/campgrounds')
+    })
+
   } catch (error) {
     console.log(error)
     req.flash("error", error.message)
@@ -26,17 +30,21 @@ router.get('/login', (req, res) => {
   res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
 
   req.flash('success', 'Welocome back')
-  res.redirect('/campgrounds')
+  const redirectUrl = res.locals.returnTo || '/campgrounds'
+  res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+  });
 })
 module.exports = router
 
-// req.logout(function (err) {
-//   if (err) {
-//       return next(err);
-//   }
-//   req.flash('success', 'Goodbye!');
-//   res.redirect('/campgrounds');
-// });
